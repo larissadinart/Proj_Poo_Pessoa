e705e5bbf7d24e3d0dd13a856f7d0d69e2c8312b
Larissa Dinart <lari_dinart@hotmail.com>
2022-08-23 23:00:45 -0300

    Início Orientação a Objetos

---
+++ Proj_Poo_Pessoa/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Poo_Pessoa
+{
+    internal class Pessoa
+    {
+        String nome;
+        String nascimento;
+        String cpf;
+        String telefone;
+
+    
+        public Pessoa(String nome, String nascimento, String cpf, String telefone)
+        {
+            this.nome = nome;
+            this.nascimento = nascimento;
+            this.cpf = cpf;
+            this.telefone = telefone;
+        }
+        public override string ToString()
+        {
+            return $"Nome: {nome} \nData Nasc: {nascimento} \nCPF: {cpf} \nTelefone: {telefone}";
+        }
+
+    }
+}
+
+
+
+
+
+
+
--- Proj_Poo_Pessoa/Program.cs
+++ Proj_Poo_Pessoa/Program.cs
@@ -6,7 +6,44 @@ namespace Proj_Poo_Pessoa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String nome = "";
+            String nascimento = "";
+            String cpf = "";
+            String telefone = "";
+
+            Pessoa[] pessoas = new Pessoa[3];
+
+
+            for(int i = 0; i < pessoas.Length; i++)
+            {
+                pessoas[i] = new Pessoa(nome,nascimento,cpf,telefone);
+
+                Console.WriteLine($"Digite o nome da pessoa {i+1}: ");
+                nome = Console.ReadLine();
+
+                Console.WriteLine($"\nDigite data de nascimento da pessoa {i+1}:");
+                nascimento = Console.ReadLine();
+
+                Console.WriteLine($"\nDigite o cpf da pessoa {i+1}: \n");
+                cpf = Console.ReadLine();
+
+                Console.WriteLine($"\nDigite o telefone da pessoa {i+1}: \n");
+                telefone = Console.ReadLine();
+
+                Console.WriteLine();
+            }
+
+            for(int i = 0; i <pessoas.Length; i++)
+            {
+                Console.WriteLine(pessoas[i]);
+            }
+
+
+
+
+
+
+
         }
     }
 }
